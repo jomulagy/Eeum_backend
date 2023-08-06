cbe49e719f9ba3300eb25240c380905e6c793032
주현지 <hyjoo0909@naver.com>
2023-08-06 22:20:20 +0900

    feat: Age, Word Serializer 생성
    AgeSerializer
    WordCreateSerializer
    WordSerializer

--- word/serializers.py
+++ word/serializers.py
@@ -1,13 +1,58 @@
-from word.models import *
+from rest_framework import serializers
+from django.utils import timezone
+from django.conf import settings
 
-def serializeWord(entity: Word) -> dict:
-    return{
-        "id": entity.pk,
-        "title": entity.title,
-        "author": entity.author,
-        "age": entity.age,
-        "content": entity.content,
-        "create_at": entity.created_at,
-        "likes": entity.likes,
-        "view": entity.views,
-    }
+
+from word.models import * 
+from account.models import User
+
+
+class AgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Age
+        fields= ["value"]
+
+
+
+class WordCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Word
+        fields= ["title", "mean", "content"]
+
+
+class WordSerializer(serializers.ModelSerializer):
+    age= serializers.SerializerMethodField()
+    likes= serializers.SerializerMethodField()  
+    created_at= serializers.SerializerMethodField()
+    image=serializers.SerializerMethodField()
+    author=serializers.SerializerMethodField()
+    edits= serializers.SerializerMethodField()
+    my_words= serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Word
+        fields = ["title","mean", "content","age","likes", "views","created_at","image","author","edits","my_words"]
+
+    def get_age(self, obj):
+        age= AgeSerializer(obj.age.all(), many=True).data
+        return [item["value"] for item in age]
+    def get_likes(self, obj):
+        return obj.get_likes()
+    def get_created_at(self,obj):
+        return obj.created_at.strftime("%Y/%m/%d %H:%M")
+    def get_image(self, obj):
+        if obj.image:
+            return settings.HOST + obj.image.url
+        else:
+            return ""
+    def get_author(self, obj):
+        return obj.author.nickname
+    def get_edits(self, obj):
+        return EditSerializer(obj.edit_set.all().order_by("-created_at"), many=True).data[:4]
+    def get_my_words(self,obj):
+        return list(obj.author.word_set.all().order_by("-created_at").values_list('title', flat=True))[:4]
+
+
+
+
+class AuthorSerializer(serializers.ModelSerializer):

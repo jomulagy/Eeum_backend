423c0cc44c1602ad657a44fcd8eb781a458d2221
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-08-15 16:18:42 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -102,8 +102,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eeum',
+        'USER': 'admin',
+        'PASSWORD': 'kjh980309',
+        'HOST': 'eeum.cloa3ma1uwfb.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3308',
     }
 }
 
